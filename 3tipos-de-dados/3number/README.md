e7298a1de6de4c13ba81fec1ea6969210adaf356
unknown <moises775azevedo@gmail.com>
2021-06-04 17:09:18 -0300

    Number

---
+++ 3tipos-de-dados/3number/README.md
@@ -0,0 +1,8 @@
+# Number
+
+Em Java Script, *Number* é um tipo de dado numérico no formato de ponto flutuante, que possui algumas variações dentre elas:
+
+* Números inteiros
+* Números Reais - float
+* NaN - Not a Number
+* Infinity - Infinito
---
+++ 3tipos-de-dados/3number/script.js
@@ -0,0 +1,11 @@
+// Integer
+console.log(4)
+
+// Float
+console.log(1.5)
+
+// NaN
+console.log(65 / "auiop") /* Não faz sentido dividir um númeri por um string, então o computador entede que não é um número. */
+
+// Infinity
+console.log(Infinity)
