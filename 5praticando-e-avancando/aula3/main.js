d3367af7a7a9f1bef19caa6d6f1470028fb3a983
Moisés Silva de Azevedo <moises775azevedo@gmail.com>
2021-08-11 16:51:28 -0300

    Aula 3

---
+++ 5praticando-e-avancando/aula3/main.js
@@ -0,0 +1,10 @@
+// continuação
+let name, age
+name = "Moises";
+age = 17;
+
+// Concatenando texto + variável
+console.log("Eu me chamo " + name + ", tenho " + age + " anos.")
+
+// Interpolando valores - usamos as cráses `` e "embalamos" a variável com esse sinal -> ${variável}
+console.log(`Meu nome é ${name} e tenho ${age} anos.`)
